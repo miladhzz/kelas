4bf7ea0136039de09f6c4303016fcff07049c64c
miladhzz <miladhzz@gmail.com>
2026-09-05 18:06:42 +0330

    se2 oop mp3

---
+++ 03-shi-graii-fesharde/jalse-02/oop_mp3/se2.py
@@ -0,0 +1,26 @@
+class Mobile:
+  serial_number = 123
+
+  def __init__(self, brand, color):
+    self.brand = brand
+    self.color = color
+    self.__battery = 100
+    self._aaa = 50
+
+  def test1(self):
+    print("testtttt1")
+
+    
+class SmartPhone(Mobile):
+  def __init__(self, brand, color, os):
+    super().__init__(brand, color)    
+    self.os = os
+
+  def test_protected(self):
+    print(self._aaa)
+
+
+mobile1 = SmartPhone("samsung a54", "black", "android 16")
+print(mobile1.color)
+mobile1._aaa = 51
+print(mobile1._aaa)
